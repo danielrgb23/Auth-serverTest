4e0ea116a76df511a3feacbad7125c21e36cf30b
Daniel <danielrgb2@gmail.com>
2022-12-28 09:28:59 -0300

    feat: recuperando estado da autenticação

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from "nookies";
 import Router from 'next/router';
 
 import { api } from "../services/api";
@@ -30,6 +31,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
  const [user, setUser] = useState<User>();
  const isAuthenticated = !!user;
 
+ useEffect(() => {
+  const { 'nextauth.token': token } = parseCookies()
+
+  if (token) {
+   api.get('/me').then(response => {
+    const { email, permissions, roles } = response.data
+
+    setUser({ email, permissions, roles })
+   })
+  }
+ }, []);
+
  async function signIn({ email, password }: SignInCredentials) {
   try {
    const response = await api.post('sessions', {
@@ -37,13 +50,27 @@ export function AuthProvider({ children }: AuthProviderProps) {
     password
    })
 
-   const { permissions, roles, token, refreshToken } = response.data
+   const { permissions, roles, token, refreshToken } = response.data;
+
+   setCookie(undefined, 'nextauth.token', token,
+    {
+     maxAge: 60 * 60 * 24 * 30,
+     path: '/'
+    })
+
+   setCookie(undefined, 'nextauth.refreshToken', refreshToken,
+    {
+     maxAge: 60 * 60 * 24 * 30,
+     path: '/'
+    })
 
    setUser({
     email,
     permissions,
     roles
    })
+
+   api.defaults.headers['Authorization'] = `Bearer ${token}`
 
    Router.push('/dashboard')
   } catch (err) {
