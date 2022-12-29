bcfeddf4b8fb89d4fd2085bc696783b2372eaee3
Daniel <danielrgb2@gmail.com>
2022-12-29 18:08:19 -0300

    feat: validando visitantes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import { FormEvent, useContext, useState } from 'react'
+import { GetServerSideProps } from 'next';
+import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
 
 import { AuthContext } from '../contexts/AuthContext';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -35,3 +38,13 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx: any) => {
+  // console.log(ctx.serverSide)
+
+  return {
+    props: {
+      
+    }
+  }
+})
